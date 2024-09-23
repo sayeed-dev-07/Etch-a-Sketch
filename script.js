3416890772e5642f551477924caa84a000319c56
sayeed <174852798+sayeed-dev-07@users.noreply.github.com>
2024-09-23 19:57:38 +0600

    nothing done

--- script.js
+++ script.js
@@ -1,15 +1,71 @@
 const container = document.querySelector('.container');
-function createDiv() {
-    for (let i = 0; i < 16; i++) {
-        const div = document.createElement('div');
-        div.className = 'innerDiv';
-        container.appendChild(div)
-        for (let j = 0; i < 16; j++) {
-            const div2 = document.createElement('div');
-            div2.className = 'childDiv'
-            const innerDiv2 = document.querySelector('.innerDiv');
-            innerDiv2.appendChild(div2)
+const chooseBtn = document.querySelector('.choose');
+const resetBtn = document.querySelector('.reset');
+
+
+
+function createDivs(amount) {
+    
+    container.innerHTML = '';
+
+    
+    const containerWidth = container.clientWidth;
+    const containerHeight = 680; 
+    const totalDivs = amount * amount;
+
+    
+    const childSize = Math.floor(Math.min(containerWidth / amount, containerHeight / amount));
+
+    
+    for (let i = 0; i < amount; i++) {
+        const innerDiv = document.createElement('div');
+        innerDiv.className = 'innerDiv';
+        container.appendChild(innerDiv);
+
+        for (let j = 0; j < amount; j++) {
+            const childDiv = document.createElement('div');
+            childDiv.className = 'childDiv';
+            childDiv.style.width = `${childSize}px`;
+            childDiv.style.height = `${childSize}px`;
+            innerDiv.appendChild(childDiv);
         }
     }
 }
-createDiv()
+
+
+
+
+chooseBtn.addEventListener('click',()=>{
+    const unserInput = prompt("Enter the number of rows and coloumns you want : ")
+    if (unserInput > 100) {
+        alert('You cant create rows and coloumns more than 100 !!')
+    }else if(unserInput <= 0){
+        alert('Enter a valid number !!')
+    }else{
+        createDivs(unserInput);
+        const childDivs = document.querySelectorAll('.childDiv');
+        childDivs.forEach(divs => {
+            divs.addEventListener('mouseenter', (e)=>{
+                divs.style.backgroundColor = getRandomColor();
+            })
+        })
+    }
+    
+})
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function resetAll() {
+    const childDivs = document.querySelectorAll('.childDiv');
+    childDivs.forEach(div => {
+        div.style.backgroundColor = '#ffffff'
+    })
+}
+resetBtn.addEventListener('click',resetAll)
